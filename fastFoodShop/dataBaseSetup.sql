cb50665beee2347611461af9b489b4d47572bb54
Lil-YH <73232658+Lil-YH@users.noreply.github.com>
2022-03-03 22:22:00 +0800

    Burger Query and Updated database

--- fastFoodShop/dataBaseSetup.sql
+++ fastFoodShop/dataBaseSetup.sql
@@ -5,16 +5,19 @@ use fastfoodshop;
 drop table if exists food;
 create table food (
   id     int,
+  foodtype  varchar(50),
   foodItem  varchar(50),
   calories int,
   price  float,
   qty    int,
   primary key (id));
  
-insert into food values (1001, 'Hamburger', 450, 5.50, 5);
-insert into food values (1002, 'Fries Large', 500, 3.00, 5);
-insert into food values (1003, 'Fries Small', 230, 2.00, 5);
-insert into food values (1004, 'Coke Large', 290, 2.50, 5);
-insert into food values (1005, 'Coke Small', 140, 1.90, 5);
+insert into food values (1001, 'Burger','Cheese Burger', 450, 5.50, 5);
+insert into food values (1001, 'Burger','Fish Burger', 450, 5.50, 5);
+insert into food values (1001, 'Burger','Chicken Burger', 450, 5.50, 5);
+insert into food values (1002, 'Fries','Fries Large', 500, 3.00, 5);
+insert into food values (1003, 'Fries','Fries Small', 230, 2.00, 5);
+insert into food values (1004, 'Drink','Coke Large', 290, 2.50, 5);
+insert into food values (1005, 'Drink','Coke Small', 140, 1.90, 5);
  
 select * from food;
