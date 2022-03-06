d3ecfb2aa3676f25856d77350e3458f3c3051200
Lil-YH <73232658+Lil-YH@users.noreply.github.com>
2022-03-06 20:09:03 +0800

    added order_records database

--- fastFoodShop/dataBaseSetup.sql
+++ fastFoodShop/dataBaseSetup.sql
@@ -2,6 +2,11 @@ create database if not exists fastfoodshop;
  
 use fastfoodshop;
  
+drop table if exists order_records;
+create table order_records (
+  id int,
+  qty_ordered int
+);
 drop table if exists food;
 create table food (
   id     int,
@@ -13,11 +18,11 @@ create table food (
   primary key (id));
  
 insert into food values (1001, 'Burger','Cheese Burger', 450, 5.50, 5);
-insert into food values (1001, 'Burger','Fish Burger', 450, 5.50, 5);
-insert into food values (1001, 'Burger','Chicken Burger', 450, 5.50, 5);
-insert into food values (1002, 'Fries','Fries Large', 500, 3.00, 5);
-insert into food values (1003, 'Fries','Fries Small', 230, 2.00, 5);
-insert into food values (1004, 'Drink','Coke Large', 290, 2.50, 5);
-insert into food values (1005, 'Drink','Coke Small', 140, 1.90, 5);
+insert into food values (1002, 'Burger','Fish Burger', 450, 5.50, 5);
+insert into food values (1003, 'Burger','Chicken Burger', 450, 5.50, 5);
+insert into food values (1004, 'Fries','Fries Large', 500, 3.00, 5);
+insert into food values (1005, 'Fries','Fries Small', 230, 2.00, 5);
+insert into food values (1006, 'Drink','Coke Large', 290, 2.50, 5);
+insert into food values (1007, 'Drink','Coke Small', 140, 1.90, 5);
  
 select * from food;
